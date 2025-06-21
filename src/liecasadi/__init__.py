d7a7b3a059227a5237f6785b3b8c509500802707
edward.ix <edmorgangh@gmail.com>
2025-06-21 11:13:45 -0700

    Add S1 manifold

--- src/liecasadi/__init__.py
+++ src/liecasadi/__init__.py
@@ -2,4 +2,5 @@ from . import hints
 from .quaternion import Quaternion
 from .so3 import SO3, SO3Tangent
 from .se3 import SE3, SE3Tangent
+from .s1 import S1, S1Tangent
 from .dualquaternion import DualQuaternion
